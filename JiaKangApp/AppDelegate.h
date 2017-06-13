95f25f3bd61c607321cd017ab609b4055d252024
Anson Ng <cwnga@yahoo-inc.com>
2017-06-13 23:08:57 +0800

    init project

---
+++ JiaKangApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JiaKangApp
+//
+//  Created by  Anson Ng on 13/06/2017.
+//  Copyright © 2017 JiaKang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
